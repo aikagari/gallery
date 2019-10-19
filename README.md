c547be27704cb4afb673becb5fe5009393bb0c7b
Константин Печенкин <tx3d@mail.ru>
2019-10-19 23:00:54 +0300

    Add first version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # gallery
 Simple local gallery
+
+# How to use
+Install npm modules
+```
+cd static
+npm init
+npm install
+```
+Create and edit your config
+```
+cat config.yaml.tmpl > config.yaml
+```
+Run
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,57 @@
+from os import listdir
+from os.path import isfile, join
+from flask import Flask, render_template, send_file, abort
+import yaml
+import get_image_size
+
+
+try:
+    with open('config.yaml', 'r') as yml_file:
+        cfg = yaml.load(yml_file)
+except (FileNotFoundError, IOError) as e:
+    raise e
+
+picture_path = cfg['path']['image']
+video_path = cfg['path']['video']
+
+video = []
+picture = []
+video_ext = ['webm', 'mp4']
+images_ext = ['jpg', 'jpeg', 'gif', 'png']
+app = Flask(__name__)
+
+for f in sorted(listdir(picture_path)):
+    if isfile(join(picture_path, f)) and f.split('.')[-1].lower() in images_ext:
+        img = get_image_size.get_image_metadata(join(picture_path, f))
+        picture.append(
+            {'name': f, 'width': img.width, 'height': img.height})
+
+
+for f in sorted(listdir(video_path)):
+    if isfile(join(video_path, f)) and f.split('.')[-1].lower() in video_ext:
+        video.append({'name': f, 'ext': f.split('.')[-1].lower()})
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', file_list={'picture': picture, 'video': video})
+
+
+@app.route('/get_image/<name>')
+def get_image(name):
+    try:
+        return send_file(picture_path + name)
+    except FileNotFoundError:
+        abort(404)
+
+
+@app.route('/get_video/<name>')
+def get_video(name):
+    try:
+        return send_file(video_path + name)
+    except FileNotFoundError:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(host=cfg['host'])
